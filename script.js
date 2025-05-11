de1379999224efaecf283ec61b5e5010a1dbe322
theaebhu09 <parmaraebhu@gmail.com>
2025-05-11 16:14:28 +0530

    index html

--- script.js
+++ script.js
@@ -16,3 +16,31 @@ productCards.forEach(card => {
         }, 300); // Remove class after 300 milliseconds
     });
 });
+const sliderContainer = document.getElementById('sliderContainer');
+const slides = document.querySelectorAll('.slide');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+let currentIndex = 0;
+
+function showSlide(index) {
+    if (index < 0) {
+        currentIndex = slides.length - 1;
+    } else if (index >= slides.length) {
+        currentIndex = 0;
+    } else {
+        currentIndex = index;
+    }
+
+    const offset = -currentIndex * 100;
+    sliderContainer.style.transform = `translateX(${offset}%)`;
+}
+
+prevBtn.addEventListener('click', () => {
+    showSlide(currentIndex - 1);
+});
+
+nextBtn.addEventListener('click', () => {
+    showSlide(currentIndex + 1);
+});
+
+showSlide(currentIndex);
